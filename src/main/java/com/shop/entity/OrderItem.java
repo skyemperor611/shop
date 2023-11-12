3ef2a31fc3d7dc9aebb917fbd3581550c4573867
skyemperor611 <blackmonarch124@gmail.com>
2023-11-12 20:29:18 +0900

    카트 및 주문 추가

---
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+
+    private int count;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
